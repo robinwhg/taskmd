4f38691db5c48241ed64a33c4840f287822f87aa
robinwhg <mail@robinwhg.com>
2026-01-26 15:13:27 +0100

    feat: parse h2 rows as columns

--- internal/markdown/parser.go
+++ internal/markdown/parser.go
@@ -2,13 +2,43 @@
 package markdown
 
 import (
+	"bufio"
 	"io"
+	"strings"
 )
 
-type Board struct{}
+const (
+	columnPrefix = "## "
+)
+
+type Column struct {
+	Name string
+}
+
+type Board struct {
+	Columns []Column
+}
 
 func Parse(input io.Reader) (*Board, error) {
-	board := Board{}
+	board := Board{Columns: ParseColumns(input)}
 
 	return &board, nil
+}
+
+func ParseColumns(input io.Reader) []Column {
+	scanner := bufio.NewScanner(input)
+
+	columns := []Column{}
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		after, found := strings.CutPrefix(text, columnPrefix)
+
+		if found {
+			columns = append(columns, Column{Name: after})
+		}
+	}
+
+	return columns
 }
--- internal/markdown/parser_test.go
+++ internal/markdown/parser_test.go
@@ -1,21 +1,67 @@
 package markdown_test
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 
 	markdown "github.com/robinwhg/taskmd/internal/markdown"
 )
 
-const input = `Hello world`
+const input = `
+## To Do
+
+## In Progress
+
+## In Review
+
+## Done
+`
 
 func TestReadInput(t *testing.T) {
 	board, err := markdown.Parse(strings.NewReader(input))
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
+
+	assertNoError(t, err)
 
 	if board == nil {
 		t.Fatal("Expected non-nil value for Board")
+	}
+}
+
+func TestParseColumns(t *testing.T) {
+	board, err := markdown.Parse(strings.NewReader(input))
+
+	assertNoError(t, err)
+
+	t.Run("Create columns", func(t *testing.T) {
+		got := len(board.Columns)
+
+		want := 4
+
+		if got != want {
+			t.Fatalf("expected %v columns, found %v", want, got)
+		}
+	})
+
+	t.Run("Create columns with title", func(t *testing.T) {
+		got := board.Columns
+
+		want := []markdown.Column{
+			{Name: "To Do"},
+			{Name: "In Progress"},
+			{Name: "In Review"},
+			{Name: "Done"},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("expected columns %v got %v", want, got)
+		}
+	})
+}
+
+func assertNoError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error, but didn't want one")
 	}
 }
