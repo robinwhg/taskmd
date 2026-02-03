aa1ef3fad00f0f4af3d54dafe2b9c0cb68e8f845
robinwhg <mail@robinwhg.com>
2026-02-03 21:59:34 +0100

    feat: create board package

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,2 +1,8 @@
 // Package board handles mutations
 package board
+
+import "github.com/robinwhg/taskmd/internal/markdown"
+
+func ToggleTask(task *markdown.Task) {
+	task.Checked = !task.Checked
+}
--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -1,1 +1,18 @@
 package board_test
+
+import (
+	"testing"
+
+	"github.com/robinwhg/taskmd/internal/board"
+	"github.com/robinwhg/taskmd/internal/markdown"
+)
+
+func TestToggleTask(t *testing.T) {
+	got := markdown.Task{Checked: false}
+	board.ToggleTask(&got)
+	want := true
+
+	if got.Checked != want {
+		t.Errorf("got %v, expected %v", got.Checked, want)
+	}
+}
