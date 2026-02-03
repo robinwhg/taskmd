ee25c8bfab0539b83d4fa88f8c3c65a0185491e7
robinwhg <mail@robinwhg.com>
2026-02-03 21:00:29 +0100

    feat: render task string

--- internal/markdown/renderer.go
+++ internal/markdown/renderer.go
@@ -3,3 +3,11 @@ package markdown
 func RenderColumn(column Column) string {
 	return columnPrefix + column.Name
 }
+
+func RenderTask(task Task) string {
+	if task.Checked {
+		return checkedTaskPrefix + task.Name
+	} else {
+		return taskPrefix + task.Name
+	}
+}
--- internal/markdown/renderer_test.go
+++ internal/markdown/renderer_test.go
@@ -15,3 +15,25 @@ func TestRenderColumn(t *testing.T) {
 		t.Errorf("got %q, expected %q", got, want)
 	}
 }
+
+func TestRenderTask(t *testing.T) {
+	t.Run("Render unchecked task", func(t *testing.T) {
+		input := markdown.Task{Name: "Task A", Checked: false}
+		got := markdown.RenderTask(input)
+		want := "- [ ] Task A"
+
+		if got != want {
+			t.Errorf("got %q, expected %q", got, want)
+		}
+	})
+
+	t.Run("Render checked task", func(t *testing.T) {
+		input := markdown.Task{Name: "Task B", Checked: true}
+		got := markdown.RenderTask(input)
+		want := "- [x] Task B"
+
+		if got != want {
+			t.Errorf("got %q, expected %q", got, want)
+		}
+	})
+}
