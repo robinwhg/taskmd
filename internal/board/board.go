be3a3a01a6e537765f6a80890e77f6408be05bd7
robinwhg <mail@robinwhg.com>
2026-02-04 20:43:36 +0100

    feat: add MoveTaskInColumn function

--- internal/board/board.go
+++ internal/board/board.go
@@ -25,3 +25,9 @@ func MoveTaskInColumn(column *markdown.Column, fromIndex, toIndex int) {
 func RenameTask(task *markdown.Task, name string) {
 	task.Name = name
 }
+
+func MoveTaskInColumn(column *markdown.Column, fromIndex, toIndex int) {
+	task := column.Tasks[fromIndex]
+	column.Tasks = slices.Delete(column.Tasks, fromIndex, fromIndex+1)
+	column.Tasks = slices.Insert(column.Tasks, toIndex, task)
+}
