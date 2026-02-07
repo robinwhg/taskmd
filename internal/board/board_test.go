2cd46aa3273226f0446370adab38d061f2e3a795
robinwhg <mail@robinwhg.com>
2026-02-07 17:20:51 +0100

    refactor: move all task move tests in table

--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -46,108 +46,80 @@ func TestRenameTask(t *testing.T) {
 }
 
 /* TODO: Test
-index out of bounds
 empty slice
 single task in slice
-refactor
 check move up and down
 */
 
 func TestMoveTaskInColumn(t *testing.T) {
-	t.Run("Move task in column", func(t *testing.T) {
-		got := markdown.Column{Tasks: []markdown.Task{
-			{Name: "Task A"},
-			{Name: "Task B"},
-		}}
+	tests := []struct {
+		name      string
+		column    markdown.Column
+		fromIndex int
+		toIndex   int
+		want      markdown.Column
+		err       error
+	}{
+		{
+			"Move task up in column",
+			makeCol("Task A", "Task B"),
+			0,
+			1,
+			makeCol("Task B", "Task A"),
+			nil,
+		},
+		{
+			"Move task to its current index",
+			makeCol("Task A"),
+			0,
+			0,
+			makeCol("Task A"),
+			nil,
+		},
+		{
+			"fromIndex out of lower bounds",
+			makeCol("Task A"),
+			-1,
+			0,
+			makeCol("Task A"),
+			board.ErrNilTask,
+		},
+		{
+			"toIndex out of lower bounds",
+			makeCol("Task A"),
+			0,
+			-1,
+			makeCol("Task A"),
+			board.ErrNilTask,
+		},
+		{
+			"fromIndex out of upper bounds",
+			makeCol("Task A"),
+			2,
+			0,
+			makeCol("Task A"),
+			board.ErrNilTask,
+		},
+		{
+			"toIndex out of upper bounds",
+			makeCol("Task A"),
+			0,
+			2,
+			makeCol("Task A"),
+			board.ErrNilTask,
+		},
+	}
 
-		want := markdown.Column{Tasks: []markdown.Task{
-			{Name: "Task B"},
-			{Name: "Task A"},
-		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := board.MoveTaskInColumn(&tt.column, tt.fromIndex, tt.toIndex)
+			assertError(t, err, tt.err)
 
-		err := board.MoveTaskInColumn(&got, 0, 1)
-
-		assertNoError(t, err)
-
-		if !reflect.DeepEqual(got.Tasks, got.Tasks) {
-			t.Errorf("got %v, expected %v", got.Tasks, want.Tasks)
-		}
-	})
-
-	t.Run("Move task to its current index", func(t *testing.T) {
-		got := markdown.Column{Tasks: []markdown.Task{
-			{Name: "Task A"},
-			{Name: "Task B"},
-		}}
-
-		want := markdown.Column{Tasks: []markdown.Task{
-			{Name: "Task A"},
-			{Name: "Task B"},
-		}}
-
-		err := board.MoveTaskInColumn(&got, 0, 0)
-
-		assertNoError(t, err)
-
-		if !reflect.DeepEqual(got.Tasks, want.Tasks) {
-			t.Errorf("got %v, expected %v", got.Tasks, want.Tasks)
-		}
-	})
-
-	t.Run("Move task with fromIndex out of bounds", func(t *testing.T) {
-		tests := []struct {
-			name      string
-			column    markdown.Column
-			fromIndex int
-			toIndex   int
-			want      markdown.Column
-			err       error
-		}{
-			{
-				"fromIndex out of lower bounds",
-				makeCol("Task A"),
-				-1,
-				0,
-				makeCol("Task A"),
-				board.ErrNilTask,
-			},
-			{
-				"toIndex out of lower bounds",
-				makeCol("Task A"),
-				0,
-				-1,
-				makeCol("Task A"),
-				board.ErrNilTask,
-			},
-			{
-				"fromIndex out of upper bounds",
-				makeCol("Task A"),
-				2,
-				0,
-				makeCol("Task A"),
-				board.ErrNilTask,
-			},
-			{
-				"toIndex out of upper bounds",
-				makeCol("Task A"),
-				0,
-				2,
-				makeCol("Task A"),
-				board.ErrNilTask,
-			},
-		}
-
-		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				err := board.MoveTaskInColumn(&tt.column, tt.fromIndex, tt.toIndex)
-				assertError(t, err, tt.err)
-
-				if !reflect.DeepEqual(tt.column.Tasks, tt.want.Tasks) {
-					t.Errorf("got %v, expected %v", tt.column.Tasks, tt.want.Tasks)
-				}
-			})
-		}
-	})
+			if !reflect.DeepEqual(tt.column.Tasks, tt.want.Tasks) {
+				t.Errorf("got %v, expected %v", tt.column.Tasks, tt.want.Tasks)
+			}
+		})
+	}
 }
 
 func makeCol(names ...string) markdown.Column {
